6c7d8ccf760663e1e29333056e16a763249885b1
t-hirata74 <t.hirata.0463@gmail.com>
2023-01-29 23:18:18 +0900

    random-cat finish

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import type { NextPage, GetServerSideProps } from "next";
+ 
+interface CatCategory {
+  id: number;
+  name: string;
+}
+ 
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+ 
+type SearchCatImageResponse = SearchCatImage[];
+ 
+const fetchCatImage = async () => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = (await res.json()) as SearchCatImageResponse;
+  return result[0];
+};
+ 
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
+ 
+const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+ 
+  const handleClick = async () => {
+    const image = await fetchCatImage();
+    setCatImageUrl(image.url);
+  };
+ 
+  return (
+    <div>
+      <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} width={500} height="auto" />
+      </div>
+    </div>
+  );
+};
+ 
+export const getServerSideProps: GetServerSideProps<
+  IndexPageProps
+> = async () => {
+  const catImage = await fetchCatImage();
+  return {
+    props: {
+      initialCatImageUrl: catImage.url,
+    },
+  };
+};
+ 
+export default IndexPage;
